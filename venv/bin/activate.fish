599c66f8a6c87e61a3ee05bd909bd491efef20ce
irfanahassan87 <irfanahassan87@gmail.com>
2025-06-13 10:47:29 +0530

    create a Byway_Django Project

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/user/Documents/IRFANA_HASSAN/irfana/projects/Byway_Django/venv
+set -gx VIRTUAL_ENV /home/user/Byway_Django/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
